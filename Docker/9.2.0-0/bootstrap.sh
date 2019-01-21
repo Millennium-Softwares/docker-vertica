49af9a0b652a0e7e479265bfa2ca3c447e1d13c4
Mohib Hassan <mohib@millsoft.com.pk>
2019-01-22 00:52:40 +0500

    fixed local storage

--- Docker/9.2.0-0/bootstrap.sh
+++ Docker/9.2.0-0/bootstrap.sh
@@ -9,13 +9,22 @@ function vertica_shut_down() {
   /bin/su - dbadmin -c '/opt/vertica/bin/vsql -d database -c "SELECT CLOSE_ALL_SESSIONS();"'
   /bin/su - dbadmin -c '/opt/vertica/bin/vsql -d database -c "SELECT MAKE_AHM_NOW();"'
   /bin/su - dbadmin -c '/opt/vertica/bin/admintools -t stop_db -d database -i'
+  echo " -----> Vertica has now shutdown"
+}
+
+function vertica_shut_down_proper() {
+  echo " -----> Properly shut down Vertica"
+  /bin/su - dbadmin -c '/opt/vertica/bin/vsql -d database -c "SELECT CLOSE_ALL_SESSIONS();"'
+  /bin/su - dbadmin -c '/opt/vertica/bin/vsql -d database -c "SELECT MAKE_AHM_NOW();"'
+  /bin/su - dbadmin -c '/opt/vertica/bin/admintools -t stop_db -d database -i'
+  echo " -----> Vertica has now shutdown"
+  exit
 }
 
 # Intercept closing of container and proper shut down Vertica
-trap "vertica_shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+trap "vertica_shut_down_proper" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
 # Function to handle Vertica DB Restore (Working on...)
-: '
 importdb(){
     echo " -----> Restoring database"
     rm -rf /srv/vertica/db/tempbak
@@ -31,14 +40,14 @@ importdb(){
     rm -R /srv/vertica/db/tempbak
     su - dbadmin -c "/opt/vertica/bin/admintools -t start_db -d database -i"
 }
-'
+
 
 if [ -z "$(ls -A "/srv/vertica/db/data")" ]; then
   # If not data exists, create folders. Then, check DB existence
   mkdir -p /srv/vertica/db/catalog
   mkdir -p /srv/vertica/db/data
-  chown -R dbadmin:verticadba "/srv/vertica/db" 
-  if [[ ! $(su - dbadmin -c "/opt/vertica/bin/admintools -t list_allnodes" | grep vertica) ]]; then 
+  chown -R dbadmin:verticadba "/srv/vertica/db"
+  if [[ ! $(su - dbadmin -c "/opt/vertica/bin/admintools -t list_allnodes" | grep vertica) ]]; then
     # No database available -> Create one
     echo " -----> Creating an empty DB"
     su - dbadmin -c "/opt/vertica/bin/admintools -t create_db -s localhost --skip-fs-checks -d database -c /srv/vertica/db/catalog -D /srv/vertica/db/data"
@@ -46,16 +55,16 @@ if [ -z "$(ls -A "/srv/vertica/db/data")" ]; then
     # Database available but no data on folder -> Delete and create new one
     echo " -----> Delete empty DB before create a new one"
     su - dbadmin -c "/opt/vertica/bin/admintools -t drop_db -d database"
-    su - dbadmin -c "/opt/vertica/bin/admintools -t create_db -s localhost --skip-fs-checks -d database -c /srv/vertica/db/catalog -D /srv/vertica/db/data"    
+    su - dbadmin -c "/opt/vertica/bin/admintools -t create_db -s localhost --skip-fs-checks -d database -c /srv/vertica/db/catalog -D /srv/vertica/db/data"
   fi
 else
-  if [[ ! $(su - dbadmin -c "/opt/vertica/bin/admintools -t list_allnodes" | grep vertica) ]]; then 
+  if [[ ! $(su - dbadmin -c "/opt/vertica/bin/admintools -t list_allnodes" | grep vertica) ]]; then
     # Database not available but data in folder -> Try to import
-    #importdb
+    importdb
     # Unable to import from row data -> Inconsistent Epoch state -> So, delete data and create new DB
-    echo " -----> Remove old DB data and create an empty one"
-    rm -rf /srv/vertica/db/*
-    su - dbadmin -c "/opt/vertica/bin/admintools -t create_db -s localhost --skip-fs-checks -d database -c /srv/vertica/db/catalog -D /srv/vertica/db/data"
+    # echo " -----> Remove old DB data and create an empty one"
+    #rm -rf /srv/vertica/db/*
+    #su - dbadmin -c "/opt/vertica/bin/admintools -t create_db -s localhost --skip-fs-checks -d database -c /srv/vertica/db/catalog -D /srv/vertica/db/data"
   else
     # We have both data and DB -> Just start
     echo " -----> DB and data available, just start DB"
